3fc74e5251ffac7b63cfcf9af0b764a616f21398
Mike Gulline <mike@mikegulline.com>
2025-07-15 21:20:47 -0700

    type for error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,6 +1,10 @@
 import AppWrapper from './AppWrapper';
 
-const Error = ({ message }: { message: string }) => (
+interface ErrorProps {
+  message: string;
+}
+
+const Error = ({ message }: ErrorProps) => (
   <AppWrapper>
     <div className='py-4 text-red-600'>{message}</div>
   </AppWrapper>
